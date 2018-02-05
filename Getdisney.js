f2c763c50f331be22e904d0be0f02852afb2b08b
Denny Cao <dennycao202@yahoo.com>
2018-02-05 07:56:46 -0800

    Error handeling

--- Getdisney.js
+++ Getdisney.js
@@ -11,7 +11,7 @@ function waitTimes(currentTime, date) {
                 console.log("wait;" + date + ";" + currentTime + ";" + ride.name + ";" + "closed");
             }
         }
-    });
+    }, console.error);
 }
 
 function fastTimes(currentTime, date) {
@@ -41,7 +41,7 @@ function fastTimes(currentTime, date) {
                 console.log("fast;" + date + ";" + currentTime + ";" + ride.name + ";" + "closed");
             }
         }
-    });
+    }, console.error);
 }
 
 //Compare the current time to the opening time of the park and return true or false, based on if the park is open.
